acfeb2bb20c62710489ebddf5739fced02adf7a5
Rob Bavey <rob.bavey@elastic.co>
2018-06-07 12:24:59 -0400

    Add Registration for cloudwatch plugin

--- lib/registry.rb
+++ lib/registry.rb
@@ -1,6 +1,8 @@
 require "logstash/plugins/registry"
+require "logstash/inputs/cloudwatch"
 require "logstash/inputs/s3"
 require "logstash/outputs/s3"
 
+LogStash::PLUGIN_REGISTRY.add(:input, "cloudwatch", LogStash::Inputs::Cloudwatch)
 LogStash::PLUGIN_REGISTRY.add(:input, "s3", LogStash::Inputs::S3)
 LogStash::PLUGIN_REGISTRY.add(:output, "s3", LogStash::Outputs::S3)
